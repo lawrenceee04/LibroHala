cebee872508d808ed4df030de35d260fe942abac
lawrenceee04 <lawrencelg04@gmail.com>
2024-11-30 23:56:24 +0800

    Rudimentary implementations for updating book information

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,5 +32,41 @@ Route::middleware('auth')->group(function () {
 Route::controller(BookController::class)->group(function () {
     Route::get('/inventory/books', 'index');
 });
+
+//// Add new book
+//Route::post('/cataloguing/books/create', function () {
+////    return view('books.create');
+//});
+
+// Update
+Route::patch('/books/{id}', function ($id) {
+    // validate
+    // authorize
+    // update
+    $book = Book::findOrFail($id);
+
+    $book->update([
+        'accession_number' => request('accession_number'),
+        'title' => request('title'),
+        'edition' => request('edition'),
+        'author' => request('author'),
+        'publisher' => request('publisher'),
+        'isbn' => request('isbn'),
+        'class' => request('class'),
+        'topic_area' => request('topic_area'),
+        'cutter_number' => request('cutter_number'),
+        'publication_year' => request('publication_year'),
+        'copies' => request('copies'),
+        'genre' => request('genre'),
+        'description' => request('description')
+    ]);
+    // persist
+    // redirect to the inventory
+//    redirect()->route();
+});
+
+//Route::delete('/books/{id}', function ($id) {
+////
+//});
 
 require __DIR__.'/auth.php';
