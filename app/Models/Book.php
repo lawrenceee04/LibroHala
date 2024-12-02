c8600196be35221d7647b7c50fe289b6136bf9f3
lawrenceee04 <lawrencelg04@gmail.com>
2024-12-02 13:35:25 +0800

    added soft delete to retain book data

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Book extends Model {
+
+class Book extends Model
+{
     protected $table = 'books';
 
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'accession_number',
@@ -27,5 +31,4 @@ class Book extends Model {
         'genre',
         'description'
     ];
-
 }
