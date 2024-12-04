1e9381ee4d46113277c42d50078017e6b925ec82
lawrenceee04 <lawrencelg04@gmail.com>
2024-12-04 22:30:38 +0800

    Book model is now searchable

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,14 +6,42 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Laravel\Scout\Searchable;
 
 class Book extends Model
 {
     protected $table = 'books';
 
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes, Searchable;
+
+    public function searchableAs()
+    {
+        return 'books_index';
+    }
+
+    public function toSearchableArray()
+    {
+        // All columns searchable
+        // $array = $this->toArray();
+        // return $array;
+
+        return [
+            'accession_number' => $this->accession_number,
+            'title' => $this->title,
+            // 'edition'=> $this->edition,
+            'author' => $this->author,
+            'publisher' => $this->publisher,
+            'isbn' => $this->isbn,
+            // 'class' => $this->class,
+            'topic_area' => $this->topic_area,
+            // 'cutter_number' => $this->cutter_number,
+            'publication_year' => $this->publication_year,
+            // 'copies' => $this->copies,
+            // 'status' => $this->status,
+            'genre' => $this->genre,
+            'description' => $this->description
+        ];
+    }
 
     protected $fillable = [
         'accession_number',
