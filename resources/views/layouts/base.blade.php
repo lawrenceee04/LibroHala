51722c43aedd5cc3295371fbc537bf758aeb0216
lawrenceee04 <lawrencelg04@gmail.com>
2024-12-23 19:06:41 +0800

    navigation link new style

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,19 +14,17 @@
 <body>
     <nav class="fixed flex justify-between items-center w-full bg-sky-700 text-white border-slate-600 p-3">
         <div class="flex">
-            <a href="#" class="flex">
+            <a href="{{ route('welcome') }}" class="flex">
                 <img src="{{@asset('favicon.ico')}}" class="h-8 ms-1" alt="" srcset="">
                 <span class="ms-2 self-center text-xl font-semibold">LibroHala</span>
             </a>
         </div>
 
         <div class="flex">
-            <a class="hover:bg-sky-900 text-neutral-50 text-sm px-2 py-3 rounded-md"
+            <a class="text-neutral-50 hover:text-stone-700 hover:bg-stone-200 text-md font-semibold uppercase px-2 py-3 rounded-md"
                 href="{{ route('kiosk.checkinout.index') }}">Kiosk</a>
-            <a class="hover:bg-sky-900 text-neutral-50 text-sm px-2 py-3 rounded-md"
+            <a class="text-neutral-50 hover:text-stone-700 hover:bg-stone-200 text-md font-semibold uppercase px-2 py-3 rounded-md"
                 href="{{ route('login') }}">Login</a>
-            {{-- <a class="hover:bg-sky-900 text-neutral-50 text-sm px-2 py-3 rounded-md"
-                href="{{ route('register') }}">Register</a> --}}
         </div>
     </nav>
     @yield('body')
